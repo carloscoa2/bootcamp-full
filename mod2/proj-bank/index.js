e69b4c8f32881850d8466a1c006b9ccf4dba5ae4
Carlos <carloscoa2@yahoo.com.br>
2020-12-17 15:41:47 -0300

    app bank - incluindo tratamento de erros, logs e validacao de campos

--- mod2/proj-bank/index.js
+++ mod2/proj-bank/index.js
@@ -1,10 +1,32 @@
 import express from 'express';
+import winston from 'winston';
 import accountsRouter from './routes/accounts.js';
 import { promises as fs } from 'fs';
 
 const { readFile, writeFile } = fs;
 
 global.filename = 'accounts.json';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    // prettier-ignore
+    new (winston.transports.Console)(),
+    // prettier-ignore
+    new (winston.transports.File)({ filename: 'my-bank-api.log' }),
+  ],
+  // prettier-ignore
+  format: combine(
+    label({ label: 'my-bank-api' }),
+    timestamp(),
+    myFormat
+  ),
+});
 
 const app = express();
 app.use(express.json());
@@ -14,7 +36,7 @@ app.use('/accounts', accountsRouter);
 app.listen(3000, async () => {
   try {
     await readFile(global.filename);
-    console.log('API started!');
+    logger.info('API started!');
   } catch (error) {
     const initialJson = {
       nextId: 1,
@@ -22,10 +44,10 @@ app.listen(3000, async () => {
     };
     writeFile(global.filename, JSON.stringify(initialJson))
       .then(() => {
-        console.log('API started and file created!');
+        logger.info('API started and file created!');
       })
       .catch((err) => {
-        console.log(err);
+        logger.error(err);
       });
   }
 });
--- mod2/proj-bank/routes/accounts.js
+++ mod2/proj-bank/routes/accounts.js
@@ -5,45 +5,58 @@ const { readFile, writeFile } = fs;
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
+
+    if (!account.name || account.balance == null) {
+      throw new Error('Name e Balance são obrigatórios');
+    }
+
     const data = JSON.parse(await readFile(global.filename));
 
-    account = { id: data.nextId++, ...account };
+    account = {
+      id: data.nextId++,
+      name: account.name,
+      balance: account.balance,
+    };
     data.accounts.push(account);
 
     await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
+
+    logger.info(`POST /accounts - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
     delete data.nextId;
     res.send(data);
+    logger.info('GET /accounts');
   } catch (error) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
     const account = data.accounts.find(
       (account) => account.id === parseInt(req.params.id)
     );
     res.send(account);
+    logger.info('GET /accounts/:id');
   } catch (error) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.filename));
     data.accounts = data.accounts.filter(
@@ -51,43 +64,70 @@ router.delete('/:id', async (req, res) => {
     );
     await writeFile(global.filename, JSON.stringify(data, null, 2));
     res.end();
+    logger.info(`DELETE /accounts/:id - ${req.params.id}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.put('/', async (req, res) => {
+router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
+
+    if (!account.id || !account.name || account.balance == null) {
+      throw new Error('ID, Name e Balance são obrigatórios');
+    }
 
     const data = JSON.parse(await readFile(global.filename));
     const index = data.accounts.findIndex((a) => a.id === account.id);
 
-    data.accounts[index] = account;
+    if (index === -1) {
+      throw new Error('Registro não encontrado.');
+    }
 
-    await writeFile(global.filename, JSON.stringify(data));
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(account);
+
+    logger.info(`PUT /accounts - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.patch('/updateBalance', async (req, res) => {
+router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
 
+    if (!account.id || account.balance == null) {
+      throw new Error('ID e Balance são obrigatórios');
+    }
+
     const data = JSON.parse(await readFile(global.filename));
     const index = data.accounts.findIndex((a) => a.id === account.id);
+
+    if (index === -1) {
+      throw new Error('Registro não encontrado.');
+    }
 
     data.accounts[index].balance = account.balance;
 
-    await writeFile(global.filename, JSON.stringify(data));
+    await writeFile(global.filename, JSON.stringify(data, null, 2));
 
     res.send(data.accounts[index]);
+
+    logger.info(`PATCH /accounts/updateBalance - ${JSON.stringify(account)}`);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
+});
+
+router.use((err, req, res, next) => {
+  logger.error(`${req.method} ${req.baseUrl} - ${err.message}`);
+  res.status(400).send({ error: err.message });
 });
 
 export default router;
